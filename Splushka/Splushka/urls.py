e262023fb810c544975f33da1e7089cdbbdfdf19
byte <banan_2007@inbox.ru>
2024-07-31 04:14:45 +1100

    little commit(dont mind it)

--- Splushka/Splushka/urls.py
+++ Splushka/Splushka/urls.py
@@ -22,5 +22,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('greet-page/', include('greetpage.urls', name='greetpage'))
+    path('greet-page/', include('greetpage.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Splushka/greetpage/views.py
+++ Splushka/greetpage/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def greet_page(request):
-    return render(request, 'greetpage/templates')
+    return render(request, 'greetpage/greetings-page.html')
